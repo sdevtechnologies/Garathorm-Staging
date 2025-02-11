4a36e301ca78514975c8185898d5d6b6506df0e8
Ayziel <ayziel00000@gmail.com>
2025-02-11 15:03:31 +0800

    Remove If else for Users

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://garathorm-staging.sdevtech.com.ph'],
 
     'allowed_origins_patterns' => [],
 
